f6012cd200ddf278e8dd40467e4d60ace4d0a6cb
XndrYG <xygarcia6406@gmail.com>
2022-01-20 08:55:34 -0800

    slide 40

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,3 +27,26 @@ module.exports.updateProduct = async function(req, res){
     });
     res.redirect(`/products/profile/${req.params.id}`);
 }
+
+module.exports.viewProducts = async function(req, res){
+    const products = await Product.findAll();
+    res.render('index', {products})
+}
+
+module.exports.renderAddForm = function(req,res){
+    const product = {
+        product_name: '',
+        description: '',
+        category: categories[0]
+    };
+    res.render('products/add', {product, categories});
+}
+
+module.exports.addProduct = async function(req, res){
+    const result = await Product.create({
+        product_name: req.body.product_name,
+        category: req.body.category,
+        description: req.body.description
+    });
+    res.redirect(`/products/profile/${result.id}`)
+}
