535cc848d044638d09518ee5364a81d6f0244140
XndrYG <xygarcia6406@gmail.com>
2022-01-20 08:24:24 -0800

    update error

---
+++ controllers/productController.js
@@ -0,0 +1,29 @@
+const {Product} = require('../models')
+const categories = ['appliances', 'home supplies', 'home goods', 'other'];
+
+module.exports.renderProfile = async function(req, res){
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/profile', {product});
+}
+
+module.exports.renderEditForm = async function (req, res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit', {product, categories});
+}
+
+module.exports.updateProduct = async function(req, res){
+    await Product.update({
+        product_name: req.body.product_name,
+        category: req.body.category,
+        description: req.body.description
+    },  {
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect(`/products/profile/${req.params.id}`);
+}
